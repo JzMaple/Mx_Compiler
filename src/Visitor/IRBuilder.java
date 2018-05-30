76ae168fe0a6e243e64ac42119120e5d0903bf82
JzMaple <Maple_jzt@sjtu.edu.cn>
2018-05-30 15:53:14 +0800

    more extended bugs

--- src/Visitor/IRBuilder.java
+++ src/Visitor/IRBuilder.java
@@ -983,18 +983,4 @@ public class IRBuilder extends MxBaseVisitor<IR> {
     static public StackAlloc getCurrent_stackAlloc() {
         return current_stackAlloc;
     }
-
-    @Override
-    public IR visitINS_STATE(MxParser.INS_STATEContext ctx) {
-        statements.add(new Label(";" + ctx.getText()));
-        visitChildren(ctx);
-        return null;
-    }
-
-    @Override
-    public IR visitEXPR_STATE(MxParser.EXPR_STATEContext ctx) {
-        statements.add(new Label(";" + ctx.getText()));
-        visitChildren(ctx);
-        return null;
-    }
 }
