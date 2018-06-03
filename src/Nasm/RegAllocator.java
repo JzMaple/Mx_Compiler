3f645462cc03e216f224e75b6bd4a109f3d15358
JzMaple <Maple_jzt@sjtu.edu.cn>
2018-06-03 21:43:51 +0800

    back again

--- src/Nasm/RegAllocator.java
+++ src/Nasm/RegAllocator.java
@@ -326,7 +326,7 @@ public class RegAllocator {
             Variable var = stackAlloc.getVar(i);
             RegX86 regX86 = RegX86.allocReg(color[i]);
             var.setReg(regX86);
-            System.out.println(var.getName() + " " /*+ regX86 + " " + var.getLife() + " " + var.getUsed() + " "*/ + i + " " + father[i]);
+//            System.out.println(var.getName() + " " /*+ regX86 + " " + var.getLife() + " " + var.getUsed() + " "*/ + i + " " + father[i]);
         }
     }
 
